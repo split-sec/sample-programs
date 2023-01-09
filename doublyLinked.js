bded0df5999049bb09d2437e3bb1bb061b7adef0
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-01-09 16:46:22 +0530

    Completed Doubly Linked List

--- doublyLinked.js
+++ doublyLinked.js
@@ -43,26 +43,26 @@ class doublyLinkedList {
             alert("Invalid position");
             return;
         } else {
-            if(pos <= count/2) {
+            if(pos <= Math.floor(this.count/2)) {
                point = this.head;
                
                while(locator != pos-1) {
-                    point = point.left;
-                    locator++;
-               }
-
-               newNode.left = point.left;
-               point.left = newNode;
-            } else {
-                point = this.tail;
-
-                while(locator != count-pos-1) {
                     point = point.right;
                     locator++;
                }
-               newNode.left = point.left;
-               point.left = newNode;
+            } else {
+                point = this.tail;
+
+                while(locator != this.count-pos) {
+                    point = point.left;
+                    locator++;
+               }
             }
+
+            newNode.right = point.right;
+            point.right = newNode;
+            newNode.left = point;
+            newNode.right.left = newNode; 
         }
 
         this.count++;
@@ -89,12 +89,22 @@ class doublyLinkedList {
 
 let dll = new doublyLinkedList();
 
-let data;
+// let data;
 
-do {
-    data = +prompt("Enter a number: ", "any number");
+// do {
+//     data = +prompt("Enter a number: ", "any number");
     
-    if(data >= 0) {
-        dll.append(data);
-    }
-}while(data >= 0);
+//     if(data >= 0) {
+//         dll.append(data);
+//     }
+// }while(data >= 0);
+
+dll.append(10);
+dll.append(20);
+dll.append(30);
+dll.append(40);
+dll.append(50);
+dll.append(60);
+dll.append(70);
+dll.insertAt(55, 5);
+dll.insertAt(5, 1);
